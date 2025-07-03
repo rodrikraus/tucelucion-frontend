810267ef692d8b64d73f8f6f2b5acc58faf72707
Rodrigo Kraus <rodrikraus10@hotmail.com>
2025-07-03 04:39:42 -0300

    Added new google cloud endpoint.

--- src/environment/Environment.tsx
+++ src/environment/Environment.tsx
@@ -1,5 +1,6 @@
 //production
-export const API_URL = "https://tucelucion-deployment.onrender.com/api";
+export const API_URL = "https://tucelucion-891669190388.southamerica-east1.run.app";
+//export const API_URL = "https://tucelucion-deployment.onrender.com/api";
 
 //development
 //const API_URL = "http://localhost:8080/api/";
